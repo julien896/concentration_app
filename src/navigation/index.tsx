94180a1104f8fdc5b770cb9fbd712912fa5e66c4
Julian <julicogui@gmail.com>
2023-08-22 22:02:24 -0300

    fix:minor navigation fix

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-d
 import { StartPage } from "../app/pages/start-page";
 import { GamePage } from "../app/pages/game-page";
 
-function RequireUser({ children }: { children: JSX.Element }) {
-  const currentUser = localStorage.getItem('concentration_user')
+const currentUser = localStorage.getItem('concentration_user');
 
+function RequireUser({ children }: { children: JSX.Element }) {
   if (!currentUser) {
     return <Navigate to="/" replace />;
   } 
@@ -17,7 +17,12 @@ export const AppNavigation = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<StartPage />} />
+          <Route 
+            path="/" 
+            element={
+              currentUser ? <Navigate to="/game" replace /> : <StartPage />
+            } 
+          />
           <Route 
             path="/game"
             element={
